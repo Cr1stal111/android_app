f31236330194fb3b5eec1058787c74e077646caf
Cr1stal111 <nikitapolyashov3@gmail.com>
2020-11-16 15:05:47 +0300

    v0.2

---
+++ app/src/main/java/com/example/app/Paint2D.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+import android.content.Context;
+
+import android.graphics.Canvas;
+import android.view.View;
+
+public class Paint2D extends View {
+    public Paint2D(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
