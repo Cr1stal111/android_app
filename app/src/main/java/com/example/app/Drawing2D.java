a2d09efccde1ab100659dfa28ee21d1287d162bb
Cr1stal111 <nikitapolyashov3@gmail.com>
2020-11-17 22:14:43 +0300

    v0.4

--- app/src/main/java/com/example/app/Drawing2D.java
+++ app/src/main/java/com/example/app/Drawing2D.java
@@ -3,6 +3,7 @@ package com.example.app;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Build;
@@ -11,19 +12,16 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-
-//import java.nio.file.Path;
 
 public class Drawing2D extends View {
 
-    private Path dPath; // позволяет рисовать и запоминать путь рисунка
+    private Path dPath; // позволяет рисовать и запоминать весь путь фигуры
 
     private Paint cPaint; // что рисуем
 
     private Paint dPaint; // как рисуем
 
-    private int pColor = 0x0008FA; // первоначальный цвет
+    private int pColor = 0x1400ed00; // первоначальный цвет
 
     private Canvas dCanvas; // сам холст
 
@@ -35,7 +33,22 @@ public class Drawing2D extends View {
 
     public Drawing2D(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        setValues();
+//      setValues();
+        current_brush_size = getResources().getInteger(R.integer.medium_size);
+
+        last_brush_size = current_brush_size;
+
+        dPath = new Path();
+
+        dPaint = new Paint();
+        dPaint.setStyle(Paint.Style.STROKE); // стиль для объекта Paint
+        dPaint.setColor(pColor); // задаём цвет
+        dPaint.setAntiAlias(true); // сглаживаем линии, рисуемые на холсте
+        dPaint.setStrokeWidth(current_brush_size); // задаём ширину кисточки
+        dPaint.setStrokeJoin(Paint.Join.ROUND); // округляет линии
+        dPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        cPaint = new Paint(Paint.DITHER_FLAG);
     }
 
     @Override
@@ -75,21 +88,7 @@ public class Drawing2D extends View {
         return true;
     }
 
-    private void setValues() {
-        current_brush_size = getResources().getInteger(R.integer.medium_size);
-
-        last_brush_size = current_brush_size;
-
-        dPath = new Path();
-
-        dPaint = new Paint();
-        dPaint.setStyle(Paint.Style.STROKE); // стиль для объекта Paint
-        dPaint.setColor(pColor); // задаём цвет
-        dPaint.setAntiAlias(true); // сглаживаем линии, рисуемые на холсте
-        dPaint.setStrokeWidth(current_brush_size); // задаём ширину кисточки
-        dPaint.setStrokeJoin(Paint.Join.ROUND); // округляет линии
-        dPaint.setStrokeCap(Paint.Cap.ROUND);
-
-        cPaint = new Paint(Paint.DITHER_FLAG);
-    }
+//    private void setValues() {
+//
+//    }
 }
